851d345d21e3831e68145f53324ac0209e99a66a
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2018-02-20 17:17:46 +0100

    travis: also test on Python 3.4 and Python 3.5

--- setup.py
+++ setup.py
@@ -80,6 +80,8 @@ setup(
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Topic :: Scientific/Engineering :: Information Analysis',
         'Topic :: Software Development :: Libraries',
